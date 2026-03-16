8f8018151e2990c40d7c3fdb5d3ceaadea976ec9
lorenzo-poderi <lorenzo.poderi@gmail.com>
2026-03-16 23:31:55 +0100

    Add files via upload

---
+++ const.js
@@ -0,0 +1,10 @@
+
+// Variabili per il GPS
+
+const MAX_RADIUS_METERS = 500; // distanza oltre la quale il cerchio è minimo
+const MIN_SIZE = 40;           // px — dimensione minima del cerchio
+const MAX_SIZE = 200;          // px — dimensione massima (quando sei sul punto)
+
+const DISTANCE_FOUND = 5;      // px — distanza dal punto
+const DISTANCE_GREEN = 40;     // px — distanza dal punto
+const DISTANCE_YELLOW = 100;   // px — distanza dal punto
